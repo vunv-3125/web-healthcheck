74833df036317fc956d9b68c6b59a7cd16e2abf2
vunv-3125 <nguyen.van.vu-c@sun-asterisk.com>
2023-10-20 16:02:13 +0700

    Fix request headers

--- healthcheck.py
+++ healthcheck.py
@@ -1,5 +1,4 @@
 import base64
-import os
 from dotenv import load_dotenv
 import requests
 from utils import is_executed_without_exception, max_retry
@@ -11,7 +10,10 @@ load_dotenv()
 @max_retry(5)
 def is_accessible(url: str, basic_auth_username: str, basic_auth_password: str):
     auth_header = generate_auth_header(basic_auth_username, basic_auth_password)
-    headers = {"Authorization": auth_header}
+    headers = {
+        "Authorization": auth_header,
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0",
+    }
     response = requests.get(url, headers=headers)
     response_code = response.status_code
 
